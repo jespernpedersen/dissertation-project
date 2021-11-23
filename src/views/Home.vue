62824b1b16b202787991a45c109fc4903c0a99b2
Diogo Geada <diogocgeada@gmail.com>
2021-11-23 17:42:16 +0000

    Fixed conflicts with master

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+      <Header id="0" title="Teste" slug="teste" :banner="require('@/assets/images/142608965-2cb17581-fbb1-48e7-92e5-b8b280276bfa.jpg')"></Header>
   </div>
 </template>
 <script>
-// @ is an alias to /src
-export default {
-  name: 'Home',
-  components: {  
+import Header from '../components/Header';
+
+  export default {
+    name: "Home",
+    components: { Header },
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    })
   }
 }
 </script>
