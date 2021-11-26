6b4aba096b0fe37ed3ac4625d6a305a3d88d1f21
Jesper Pedersen <jeppenp12@gmail.com>
2021-11-26 19:39:46 +0100

    Categories now being outputted

--- src/services/mockCategoriesService.js
+++ src/services/mockCategoriesService.js
@@ -1,11 +1,14 @@
-import { forEach } from 'core-js/core/array';
 import data from '../assets/data/dishes.json';
 
 export default class CategoryService {
     static async getAllCategories () {
-        let categories = data.filter(dish => dish.categories);
+        let categories = [];
+        data.forEach(item => {
+            categories.push(item.categories);
+        })
         return categories;
     }
+    // Will be finished once categories becomes integers instead of strings
     static async getCategory(category){
         console.log(category);
     }
