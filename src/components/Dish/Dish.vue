a1778cf1c52aaa3f0b832f53810cba8f56487237
Jesper Pedersen <jeppenp12@gmail.com>
2021-11-26 10:41:52 +0100

    Dish.vue from #34

--- src/components/Dish/Dish.vue
+++ src/components/Dish/Dish.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="dish-inner" v-bind:style="{ backgroundImage: 'url(' + image + ')' }">
+    <div class="dish-inner" :class="layout">
+        <div class="image-wrapper">
+            <img :src='image' alt="" />
+        </div>
         <div class="dish-label">
             <div class="dish-label-title">
-                <strong>{{ title }}</strong>
-                {{ description }} 
+                <h3>{{ title }}</h3>
+                <p>{{ description | descriptionLimit }}</p>
             </div>
             <div class="dish-label-price">
-                {{ price }},- 
+                <span>{{ pricetext }}</span>
             </div>
         </div>
-        <div class="dish-add-item"  @click="addDishItem(dish)">
+        <div class="dish-add-item"  @click="addDishItem(dish)" v-if="layout == 'horizontal'">
             +
         </div> 
     </div>
@@ -17,44 +20,76 @@
 <script>
 export default {
     name: 'Dish',
-    props: ['title', 'image', 'description', 'price', 'ingredients', 'special'],
     data () {
         return {
-            dishes: []
+            pricetext: ''
         }
     },
+    props: ['title', 'image', 'description', 'price', 'ingredients', 'layout'],
     methods: {
         addDishItem(id) {
             // Do functionality here for adding items to cart or something else
             console.log("Adding item " + id)
         } 
     },
+    created () {
+        if(this.price % 1 == 0) {
+            this.pricetext = this.price + ",-";
+        }
+    },
+    filters: {
+        descriptionLimit: (text) => {
+            if(text.length > 50) {
+                return text.substring(0, 50) + "...";
+            }
+            return text;
+        }
+    }
 }
 </script>
 <style scoped>
     .dish-inner {
-        width: 300px;
-        height: 200px;
         background-position: center center;
-        background-size: top auto;
         color: #33363a;
-        border-radius: 12px;
         position: relative;
         display: inline-block;
+        border-radius: 12px;
+        width: 100%;
+        height: 15vw;
+    }
+    .dish-inner.vertical .dish-label {
+        display: inline-block;
+        padding: 15px 0;
+    }
+    img {
+        height: auto;
+        width: 100%;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        border-radius: 12px 0 0 12px;
+    }
+    .image-wrapper {
+        overflow: hidden;
+        height: calc(100% / 1.78);
+        width: 100%;
+        position: relative;
     }
     .dish-label {
         background-color: #f6f5f1;
-        border-radius: 12px;
-        position: absolute;
-        bottom: 0;
+        border-radius: 0 12px 12px 0;
         display: flex;
         align-items: center;
-        justify-content: center;
-        padding: 10px 20px;
+        justify-content: space-between;
+        padding: 10px 20px 5px;
         text-align: left;
+        width: 100%;
+        min-height: 100px;
     }
     .dish-label-title {
         font-size: 14px;
+        text-align: left;
     }
     .dish-label-title strong {
         font-size: 18px;
