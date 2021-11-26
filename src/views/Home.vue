0d92bf87c2c25d585ba30b2d2c2387909e0f0653
Jesper Pedersen <jeppenp12@gmail.com>
2021-11-26 18:10:05 +0100

    Remove template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,72 +1,19 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar
-      app
-      color="white"
-      flat
-    >
-    <div class="home">
-      <Header :id="restaurant.id" :title="restaurant.title" :slug="restaurant.slug" :logo="restaurant.logo" :banner="restaurant.banner"></Header>
+  <div>
+    <Header :id="restaurant.id" :title="restaurant.title" :slug="restaurant.slug" :logo="restaurant.logo" :banner="restaurant.banner"></Header>
+    <div class="dishes">
+      <div class="dish" v-for="dish in dishes" :key="dish">
+        <Dish
+          :title="dish.title"
+          :image="dish.cover_image"
+          :description="dish.description"
+          :price="dish.price"
+          :ingredients="dish.ingredients"
+          :special="dish.isTodaysSpecial"
+        ></Dish>
+      </div>
     </div>
-    </v-app-bar>
-
-    <v-main class="grey lighten-3">
-      <v-container>
-        <v-row>
-          <v-col cols="2">
-            <v-sheet rounded="lg">
-              <v-list color="transparent">
-                <v-list-item
-                  v-for="n in 5"
-                  :key="n"
-                  link
-                >
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      List Item {{ n }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-
-                <v-divider class="my-2"></v-divider>
-
-                <v-list-item
-                  link
-                  color="grey lighten-4"
-                >
-                  <v-list-item-content>
-                    <v-list-item-title>
-                      Refresh
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-sheet>
-          </v-col>
-
-          <v-col>
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-            <div class="dishes">
-              <div class="dish" v-for="dish in dishes" :key="dish">
-                <Dish
-                  :title="dish.title"
-                  :image="dish.cover_image"
-                  :description="dish.description"
-                  :price="dish.price"
-                  :ingredients="dish.ingredients"
-                  :special="dish.isTodaysSpecial"
-                ></Dish>
-              </div>
-            </div>
-            </v-sheet>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+  </div>
 </template>
 <script>
 // Components
