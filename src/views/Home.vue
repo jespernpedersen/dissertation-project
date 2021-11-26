187b23dd912b5da7dc6d5ad572799c7bff9ba35e
Jesper Pedersen <jeppenp12@gmail.com>
2021-11-26 18:06:52 +0100

    Merge from master

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,11 +5,9 @@
       color="white"
       flat
     >
-      <v-container class="py-0 fill-height">
-        <div class="home">
-          <Header :id="restaurant.id" :title="restaurant.title" :slug="restaurant.slug" :logo="restaurant.logo" :banner="restaurant.banner"></Header>
-        </div>
-      </v-container>
+    <div class="home">
+      <Header :id="restaurant.id" :title="restaurant.title" :slug="restaurant.slug" :logo="restaurant.logo" :banner="restaurant.banner"></Header>
+    </div>
     </v-app-bar>
 
     <v-main class="grey lighten-3">
@@ -51,27 +49,6 @@
               min-height="70vh"
               rounded="lg"
             >
-            <h2>Today's Specials</h2>
-            <div class="todays-special-wrapper">
-              <div class="todays-special">
-                <v-slide-group
-                  multiple
-                  show-arrows
-                >
-                  <v-slide-item class="special-dish" v-for="dishSpecial in dishesSpecial" :key="dishSpecial">
-                    <Dish
-                        :title="dishSpecial.title"
-                        :image="dishSpecial.cover_image"
-                        :description="dishSpecial.description"
-                        :price="dishSpecial.price"
-                        :ingredients="dishSpecial.ingredients"
-                        :layout="'vertical'"
-                      ></Dish>
-                  </v-slide-item> 
-                </v-slide-group>
-              </div>
-            </div>
-            <h2>Menu</h2>           
             <div class="dishes">
               <div class="dish" v-for="dish in dishes" :key="dish">
                 <Dish
@@ -80,7 +57,7 @@
                   :description="dish.description"
                   :price="dish.price"
                   :ingredients="dish.ingredients"
-                  :layout="'horizontal'"
+                  :special="dish.isTodaysSpecial"
                 ></Dish>
               </div>
             </div>
@@ -102,6 +79,7 @@ import Header from '@/components/Header.vue'
 
 import bannerImg from '@/assets/images/142608965-2cb17581-fbb1-48e7-92e5-b8b280276bfa.jpg'
 import logoImg from '@/assets/images/142608969-32d24de8-4598-4e45-a711-bed26b9929b0.jpg'
+
 export default {
   name: 'Home',
   components: {
@@ -109,21 +87,20 @@ export default {
   },
   data () {
     return {
-      dishes: [],
-      dishesSpecial: [],
-      links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
-      ],
       restaurant: {
         id: 0,
         title: "The italian",
         slug: "the_italian",
         logo: logoImg,
         banner: bannerImg,
-      }
+      },
+      dishes: [],
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
     }
   },
   methods: {
@@ -134,50 +111,21 @@ export default {
       } catch (error) {
         console.log(error);
       }
-    },  
-    async getSpecialDishes() {
-      try {
-        const data = await DishesService.getTodaysSpecial();
-        this.dishesSpecial = data;
-      } catch (error) {
-        console.log(error);
-      }
     },    
   },
   mounted: function() {
     this.getDishes();
-    this.getSpecialDishes();
-  }
+    Header
+  },
 }
 </script>
 <style scoped>
   .dishes {
-    padding: 0px 10px;
+    padding: 30px 10px;
   }
   .dishes .dish {
     display: inline-block;
     width: 100%;
     text-align: center;
-    overflow: hidden;
-  }
-  h2 {
-    padding: 20px 0 10px;
-  }
-  h2,
-  .todays-special {
-    text-align: center;
-  }
-  .todays-special {
-    width: 50vw;
-    padding-bottom: 100px;
-  }
-  .todays-special-wrapper {
-    text-align: center;
-  }
-  .special-dish {
-    width: 300px;
-  }
-  .special-dish + .special-dish {
-    margin-left: 15px;
   }
 </style>
