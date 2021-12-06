55bd4b5d0349ea337d7b52b2d8685d27908bc6e1
Diogo Geada <diogocgeada@gmail.com>
2021-12-06 13:59:47 +0000

    Created unit and integration test for HorizontalMenu

--- src/components/HorizontalMenu.vue
+++ src/components/HorizontalMenu.vue
@@ -2,7 +2,7 @@
     <div class="todays-special-wrapper">
       <div class="todays-special">
         <h2>{{ title }}</h2>
-        <v-slide-group
+        <v-slide-group v-if="!isLoading && dishes.length > 0"
           multiple
           height="200"
           class="wrapper"
@@ -18,6 +18,14 @@
             ></Dish>
           </v-slide-item> 
         </v-slide-group>
+        <div v-else-if="isLoading">
+          <h3>Loading...</h3>
+        </div>
+        <div v-else class="error-msg">
+                <h3>Something went wrong</h3>
+                <p>There was a problem fetching the menu.</p>
+                <div><v-btn color="primary" @click="refresh" id="reload">Try again <v-icon>mdi-refresh</v-icon></v-btn> or <v-btn id="back" @click="back" color="primary"><v-icon>mdi-arrow-left</v-icon> Go back</v-btn></div>
+            </div>
       </div>    
     </div>
 </template>
@@ -28,12 +36,28 @@ import Dish from "@/components/Dish";
 export default {
     name: "HorizontalMenu",
     props: ["title", "dishes", "isLoading"],
-    components: { Dish }
+    components: { Dish },
+    methods: {
+      refresh() {
+        location.reload();
+      },
+      back() {
+        window.history.back();
+      },
+    }
 }
 </script>
 
 <style scoped>
 .wrapper{
   height: 300px;
+}
+
+.error-msg {
+    text-align: center;
+}
+
+.error-msg h3{
+    color: darkred;
 }
 </style>
