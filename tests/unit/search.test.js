4fab50c63c6842d8f70c8f4a84caeada4c6198ac
Jesper Pedersen <jeppenp12@gmail.com>
2021-12-07 15:10:09 +0100

    More progress on unit test

--- tests/unit/search.test.js
+++ tests/unit/search.test.js
@@ -14,6 +14,11 @@ describe("Search", () => {
     let vuetify = new Vuetify(vuetifyConfig);
 
     it("filters dishes", () => {
-        // 
+        let localString = {...SearchBar};
+        localString.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse porttitor feugiat aliquam. In euismod quis tellus id fringilla. Maecenas est magna, tempus eleifend pellentesque quis, porta in dolor. Cras facilisis pellentesque diam, sed cursus nulla pellentesque et. Proin sit amet feugiat augue. Ut ornare, odio ac volutpat tincidunt, lorem lorem porttitor quam, eu aliquet velit turpis imperdiet erat. Vivamus eu lacus gravida, tempor odio nec, tincidunt dui. Vestibulum et scelerisque neque, nec sodales justo. Ut laoreet ultrices nulla, sed fermentum mi eleifend quis."
+
+        let wrapper = mount(SearchBar, {
+            propsData: localString
+        });
     });
 })
