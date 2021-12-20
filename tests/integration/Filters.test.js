c5cc35167bc6b69b51a4d0a60ab0075b4653a784
Jesper Pedersen <jeppenp12@gmail.com>
2021-12-20 17:40:27 +0100

    Add alphabetically sorting test

--- tests/integration/Filters.test.js
+++ tests/integration/Filters.test.js
@@ -240,8 +240,34 @@ describe("Filters.vue", () => {
         // Expect Bake n' cake
         expect(firstFilteredItem).toBe("Bake n' cake");
     });
+    
+    it("it sorts alphabetically", async () => {
+        wrapper = await mountMenu(dishes);
+
+        // Open dialog box first
+        let filterButton = wrapper.find("#filter-button");
+        await filterButton.trigger("click");
+        
+        // range slider is not compatible with tests; it has to be bypassed
+        let filters = wrapper.findComponent(Filters);
+
+        // Default values are 15, 200
+        filters.vm.$data.range = [15,200];
+
+        // Set sorting
+        filters.vm.$data.sortBy = "Alphabetically";
+
+        // Filter Dishes
+        let filterDishesBtn = wrapper.find("#filter-dishes-btn");
+        await filterDishesBtn.trigger("click");
+
+        let filteredDishes = wrapper.findAll(".filtered-items .dish-inner");
+        let firstFilteredItem = filteredDishes.at(0).find(".dish-label-title h3").text();
+
+        // Expect Amici Di Vincenzo
+        expect(firstFilteredItem).toBe("Amici Di Vincenzo");
+    });
     /*
-    it sorts by price asc
     it sorts alphabetically
     it sorts by default
     */
