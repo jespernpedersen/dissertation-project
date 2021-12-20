7f1e09ce9ad2065f848c52c4e015f3b7c089f267
akram0171 <akra0171@edu.easj.dk>
2021-12-20 11:02:35 +0100

    list

---
+++ src/components/LowerNavbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="lowernav">
+    <a href="#" class="lowernav_link">
+    <i class="v-icon soup">mdi-bowl</i> 
+    <i class="v-icon starter">mdi-food-croissant</i> 
+    <i class="v-icon main_course">mdi-silverware-fork-knife</i> 
+    <i class="v-icon drink">mdi-mdi-beer</i> 
+    <i class="v-icon dessert">mdi-cupcake</i> 
+    </a>
+  </nav>
+</template>
+
+<script>
+  export default { 
+    name: "LowerNavbar"
+  }
+</script>
+
+<style>
+
+</style>
