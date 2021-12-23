bd2660c63edf9742245ac1bcc3828068a1474d79
Jesper Pedersen <jeppenp12@gmail.com>
2021-12-23 14:33:47 +0100

    Fix test

--- tests/unit/restaurant.test.js
+++ tests/unit/restaurant.test.js
@@ -25,11 +25,9 @@ describe('restaurant.vue', () => {
         let localRestaurant = {...restaurant}
         localRestaurant.title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse porttitor feugiat aliquam. In euismod quis tellus id fringilla. Maecenas est magna, tempus eleifend pellentesque quis, porta in dolor. Cras facilisis pellentesque diam, sed cursus nulla pellentesque et. Proin sit amet feugiat augue. Ut ornare, odio ac volutpat tincidunt, lorem lorem porttitor quam, eu aliquet velit turpis imperdiet erat. Vivamus eu lacus gravida, tempor odio nec, tincidunt dui. Vestibulum et scelerisque neque, nec sodales justo. Ut laoreet ultrices nulla, sed fermentum mi eleifend quis."
 
-        let wrapper = mount(restaurant, {
+        let wrapper = mount(Restaurant, {
             propsData: localRestaurant
         });
-
-        let title = wrapper.find('h1');
 
         expect(wrapper.find('h1').text().length).toBeLessThanOrEqual(53)
     });
